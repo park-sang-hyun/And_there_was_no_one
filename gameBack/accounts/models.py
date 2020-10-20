7f2ffd6e8d67c699efdbc6fb190a7a5555e85cf4
황신실 <ohgyosin@naver.com>
2020-10-20 18:44:17 +0900

    S03P31D105-11 [ADD] account CRUD done

--- gameBack/accounts/models.py
+++ gameBack/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    nickname = models.CharField('닉네임', max_length=40, unique=True)
+    playcount = models.IntegerField(default=0)
+    wincount = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
+    friends = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='myfriends')
