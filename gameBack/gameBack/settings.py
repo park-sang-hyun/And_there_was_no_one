644b2cd7495770ffe9050ec5eaf82d80afc1b680
황신실 <ohgyosin@naver.com>
2020-10-21 19:46:27 +0900

    S03P31D105-35 [Edit] added CHANNEL_LAYERS & ASGI_APPLICATION Redis를 통해 CHANNEL_LAYERS를 사용하면서 같은 방에 묶인 사용자들끼리만 데이터를 주고 받을 수 있음 ASGI_APPLICATION으로 기존의 http 요청 이외에도 소켓 통신을 할 수 있음

--- gameBack/gameBack/settings.py
+++ gameBack/gameBack/settings.py
@@ -121,6 +121,17 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Channels
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
