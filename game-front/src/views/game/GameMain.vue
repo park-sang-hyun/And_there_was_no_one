25d76ba7dddbd0de9c8cc885ee8e204b89eb71e6
Daeheum <conquerer1209@gmail.com>
2020-10-26 23:30:25 +0900

    S03P31D105-51 [ADD] Implementation of a drawing screen.
    1. Using Stomp Protocol.
    2. Send whenever a line is created with the mouse.
    
    Signed-off-by: Daeheum <conquerer1209@gmail.com>

--- game-front/src/views/game/GameMain.vue
+++ game-front/src/views/game/GameMain.vue
@@ -28,8 +28,11 @@
 </template>
 
 <script>
-import "../../assets/css/draw.css";
-//import http from "../../util/http-common.js";
+import "../../assets/css/draw.css"
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
+// import $ from 'jquery'
+import http from "../../util/http-common.js";
 
 export default {
     components:{
@@ -39,6 +42,10 @@ export default {
         return{
             x: 0,
             y: 0,
+            ox: 0,
+            oy: 0,
+            nx: 0,
+            ny: 0,
             isDrawing: false,
             cancas: null,
             ctx: null,
@@ -52,10 +59,11 @@ export default {
             filling: false,
             image:null,
             link:null,
+            recvList: [],
         }
     },
     created(){
-
+         this.connect()
     },
     mounted(){
         this.canvas = document.getElementById("jsCanvas");
@@ -76,23 +84,97 @@ export default {
 
         this.painting = false;
         this.filling = false;
+
     },
     methods:{
+         connect() {
+            const serverURL= "http://localhost:8000"
+            let socket = new SockJS(serverURL);
+            this.stompClient = Stomp.over(socket);
+            // console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+            this.stompClient.connect(
+                {},
+                frame => {
+                // 소켓 연결 성공
+                this.connected = true;
+                // console.log('소켓 연결 성공', frame);
+                // 서버의 메시지 전송 endpoint를 구독합니다.
+                // 이런형태를 pub sub 구조라고 합니다.
+                // console.log(this.teamId)
+                this.stompClient.subscribe("/topic/"+"1", res => {
+                 console.log("구독으로 받은 메시지 입니다.", res.body)
+
+                    // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+                    console.log(JSON.parse(res.body))
+
+                    let r = JSON.parse(res.body)
+                    
+                    this.receiveMouseMove(r.x1, r.y1, r.x2, r.y2, r.color, r.width, r.mode);
+                    
+                    //this.recvList.push(JSON.parse(res.body))
+                    // this.scrolltoBottom()
+                });
+            },
+                error => {
+                // 소켓 연결 실패
+                // console.log('소켓 연결 실패', error);
+                this.connected = false;
+                }
+            );        
+        },
+        receiveMouseMove(x1, y1, x2, y2, color, width, mode){
+            console.log("painting : ", this.painting)
+            console.log("filling : ", this.filling)
+            console.log("mode : ", mode)
+
+            this.ctx.strokeStyle = color;
+            this.ctx.fillStyle = color;
+            this.ctx.lineWidth = width;
+            
+            this.ctx.beginPath();
+            this.ctx.moveTo(x1, y1);
+            this.ctx.lineTo(x2, y2);
+            this.ctx.stroke();
+            
+        },
+        send() {
+            if (this.stompClient && this.stompClient.connected) {
+                const msg = { 
+                    x1: this.ox,
+                    y1: this.oy,
+                    x2: this.nx,
+                    y2: this.ny,
+                    color: this.ctx.fillStyle,
+                    width: this.ctx.lineWidth,
+                    mode: this.filling
+                };
+                this.stompClient.send("/app/topic/" + "1",JSON.stringify(msg),{});
+            }
+         },
         stopPainting(){
             this.painting = false;
         },
-        startPainting(){
+        startPainting(e){
             this.painting = true;
+            this.ox = e.offsetX;
+            this.oy = e.offsetY;
         },
         onMouseMove(e){
             let x = e.offsetX;
             let y = e.offsetY;
             this.x = x;
             this.y = y;
+        
+            
             if(!this.painting) {
                 this.ctx.beginPath();
                 this.ctx.moveTo(x, y);
             } else {
+                this.nx = x;
+                this.ny = y;
+                this.send();
+                this.ox = this.nx;
+                this.oy = this.ny;
                 this.ctx.lineTo(x, y);
                 this.ctx.stroke();
             }
