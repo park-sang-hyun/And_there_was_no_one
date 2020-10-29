f58619b741ec84b88293f9f01e15899b28882ba8
황신실 <ohgyosin@naver.com>
2020-10-29 19:45:31 +0900

    S03P31D105-74 [ADD] added component

---
+++ game-front/src/components/lobby/roomlist.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h5>게임방 목록</h5>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- game-front/src/views/lobby/lobby.vue
+++ game-front/src/views/lobby/lobby.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-      <h5>Lobby page</h5>
+  <div class="container">
+    <h1>Lobby page</h1>
+    <div class="gerstner-2">
+      <div class="comp">
+        <Profile :component="component" class="profile"/>
+        <Friends :component="component" class="friends"/>
+      </div>
+      <div class="comp">
+        <Roomlist :component="component" class="roomlist"/>
+        <Chat :component="component" class="chat"/>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
-export default {
+import Chat from "../../components/lobby/chat.vue";
+import Friends from "../../components/lobby/friends.vue";
+import Profile from "../../components/lobby/profile.vue";
+import Roomlist from "../../components/lobby/roomlist.vue";
 
+export default {
+  components: {
+    Chat,
+    Friends,
+    Profile,
+    Roomlist,
+  },
 }
 </script>
 
 <style>
-
+  .gerstner-2 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    /* grid-column-gap: 1em; */
+  }
+  .comp {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+  }
+  .profile { background-color: #ba68c8; }
+  .friends { background-color: #245eaa; }
+  .roomlist{ background-color: #aca002; }
+  .chat { background-color: #77aa24; }
 </style>
