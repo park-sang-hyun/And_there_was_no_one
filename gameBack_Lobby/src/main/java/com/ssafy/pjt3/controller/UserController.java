5d429681a24eb1b161900ef0557631cfbb89d15e
Daeheum <conquerer1209@gmail.com>
2020-11-01 19:43:57 +0900

    S03P31D105-73 [Add] Implement front-end chat socket communication.
    1. Create a web socket and connect it to the server.
    2. Displays messages sent and received through web sockets on the screen.
    3. Using a web socket handler, it creates a client socket and broadcasts a message.
    
    Signed-off-by: Daeheum <conquerer1209@gmail.com>

--- gameBack_Lobby/src/main/java/com/ssafy/pjt3/controller/UserController.java
+++ gameBack_Lobby/src/main/java/com/ssafy/pjt3/controller/UserController.java
@@ -1,5 +1,44 @@
 package com.ssafy.pjt3.controller;
 
-public class UserController {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.pjt3.dto.Room;
+import com.ssafy.pjt3.dto.User;
+import com.ssafy.pjt3.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/friend/make")
+	public void makeFriend() {
+		
+	}
+	
+//	@GetMapping("/friend/list") //친구 목록 보여주기(
+//	public List<User> showFriend() {
+//		List<User> list = new ArrayList<User>();
+//
+//		try {
+//			list = userService.findAll();
+//			for (int i = 0; i < list.size(); i++) {
+//				System.out.println("title: " + list.get(i).getTitle());
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//
+//		return list;
+//	}
+	
 }
