43388c1274f4b76e5f6570c9fb778d076fb8d364
sanghyunPark <park03851@naver.com>
2020-11-10 17:35:03 +0900

    S03P31D105-127 [Add] Implement the Receive Notifications feature.
    1. Implementing the Receive Notifications feature.
    
    Signed-off-by: sanghyunPark <park03851@naver.com>

--- gameBack_Lobby/src/main/java/com/ssafy/pjt3/controller/AlarmController.java
+++ gameBack_Lobby/src/main/java/com/ssafy/pjt3/controller/AlarmController.java
@@ -1,11 +1,15 @@
 package com.ssafy.pjt3.controller;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,12 +48,7 @@ public class AlarmController {
 			alarm.setTo_id(to_id);
 			alarm.setContent(content);
 			
-			System.out.println("from_id : " + from_id);
-			System.out.println("to_id : " + to_id);
-			System.out.println("content : " + content);
-			
 			alarmService.send(alarm);
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -60,5 +59,21 @@ public class AlarmController {
         return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
-	
+	@GetMapping("/receive/{user_id}")
+	@ApiOperation(value = "알림 받기", notes = "알림 받기 기능 구현")
+	public Object receive(@PathVariable int user_id) {
+		List<Alarm> alarmList = new ArrayList<>();
+		List<String> contents = new ArrayList<>();
+		
+		try {
+			alarmList = alarmService.receive(user_id);
+			
+			for(int i=0; i<alarmList.size(); i++) contents.add(alarmList.get(i).getContent());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return contents;
+	}
 }
