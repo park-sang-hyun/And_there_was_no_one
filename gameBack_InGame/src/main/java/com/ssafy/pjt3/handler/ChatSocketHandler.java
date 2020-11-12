45b5a2672fe0779bf2fc7d60df653c0ec554603d
Daeheum <conquerer1209@gmail.com>
2020-11-12 22:34:16 +0900

    [Edit] Implement game waiting room chat function.
    Signed-off-by: Daeheum <conquerer1209@gmail.com>

--- gameBack_InGame/src/main/java/com/ssafy/pjt3/handler/ChatSocketHandler.java
+++ gameBack_InGame/src/main/java/com/ssafy/pjt3/handler/ChatSocketHandler.java
@@ -1,9 +1,13 @@
 package com.ssafy.pjt3.handler;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -12,56 +16,105 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class ChatSocketHandler extends TextWebSocketHandler {
-	private Map<String, WebSocketSession> sessions = new HashMap<String, WebSocketSession>();
+	// HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘
+	// 맵
+	List<HashMap<String, Object>> rls = new ArrayList<>(); // 웹소켓 세션을 담아둘 리스트 ---roomListSessions
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println("세션아이디[" + session.getId() + "] 입장.");
-
-		Map<String, Object> map = session.getAttributes();
-		String id = (String) map.get("userId");
-		System.out.println(id);
-		
-		Iterator<String> sessionIds = sessions.keySet().iterator();
-
-		String sessionId = "";
-
-		while (sessionIds.hasNext()) {
-			sessionId = sessionIds.next();
-			sessions.get(sessionId).sendMessage(new TextMessage("[" + session.getId() + "] 입장."));
+		// 소켓 연결
+		super.afterConnectionEstablished(session);
+		boolean flag = false;
+		String url = session.getUri().toString();
+		System.out.println(url);
+		String room_id = url.split("/chating/")[1];
+		int idx = rls.size(); // 방의 사이즈를 조사한다.
+		if (rls.size() > 0) {
+			for (int i = 0; i < rls.size(); i++) {
+				String rN = (String) rls.get(i).get("room_id");
+				if (rN.equals(room_id)) {
+					flag = true;
+					idx = i;
+					break;
+				}
+			}
 		}
 
-		sessions.put(session.getId(), session);
+		if (flag) { // 존재하는 방이라면 세션만 추가한다.
+			HashMap<String, Object> map = rls.get(idx);
+			map.put(session.getId(), session);
+		} else { // 최초 생성하는 방이라면 방번호와 세션을 추가한다.
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("room_id", room_id);
+			map.put(session.getId(), session);
+			rls.add(map);
+		}
+
+//		// 세션등록이 끝나면 발급받은 세션ID값의 메시지를 발송한다.
+//		JSONObject obj = new JSONObject();
+//		obj.put("type", "getId");
+//		obj.put("sessionId", session.getId());
+//		session.sendMessage(new TextMessage(obj.toJSONString()));
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		System.out.println("세션아이디[" + session.getId() + "] 퇴장.");
-
-		sessions.remove(session.getId());
-
-		Iterator<String> sessionIds = sessions.keySet().iterator();
-
-		String sessionId = "";
-
-		while (sessionIds.hasNext()) {
-			sessionId = sessionIds.next();
-			sessions.get(sessionId).sendMessage(new TextMessage("[" + session.getId() + "] 퇴장."));
+		// 소켓 종료
+		if (rls.size() > 0) { // 소켓이 종료되면 해당 세션값들을 찾아서 지운다.
+			for (int i = 0; i < rls.size(); i++) {
+				rls.get(i).remove(session.getId());
+			}
 		}
+		super.afterConnectionClosed(session, status);
 	}
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		// 메시지 발송
+		String msg = message.getPayload();
+		JSONObject obj = jsonToObjectParser(msg);
 
-		System.out.println(session.getId() + "이(가) 메세지전송.");
+		String rN = (String) obj.get("room_id"); // 어느 방에 보낼 것 인지.
+		String data = (String) obj.get("data"); // 보낼 데이터의 내용.
+		
+		HashMap<String, Object> temp = new HashMap<String, Object>();
+		if (rls.size() > 0) {
+			for (int i = 0; i < rls.size(); i++) {
+				String roomNumber = (String) rls.get(i).get("room_id"); // 세션리스트의 저장된 방번호를 가져와서
+				if (roomNumber.equals(rN)) { // 같은값의 방이 존재한다면
+					temp = rls.get(i); // 해당 방번호의 세션리스트의 존재하는 모든 object값을 가져온다.
+					break;
+				}
+			}
 
-		Iterator<String> sessionIds = sessions.keySet().iterator();
+			// 해당 방의 세션들만 찾아서 메시지를 발송해준다.
+			for (String k : temp.keySet()) {
+				if (k.equals("room_id")) { // 다만 방번호일 경우에는 건너뛴다.
+					continue;
+				}
 
-		String sessionId = "";
-		while (sessionIds.hasNext()) {
-			sessionId = sessionIds.next();
-			sessions.get(sessionId).sendMessage(new TextMessage(session.getId() + " : " + message.getPayload()));
+				WebSocketSession wss = (WebSocketSession) temp.get(k);
+				if (wss != null) {
+					try {
+						wss.sendMessage(new TextMessage(data));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
+
+	}
+
+	private static JSONObject jsonToObjectParser(String jsonStr) {
+		JSONParser parser = new JSONParser();
+		JSONObject obj = null;
+		try {
+			obj = (JSONObject) parser.parse(jsonStr);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return obj;
 	}
 
 }
