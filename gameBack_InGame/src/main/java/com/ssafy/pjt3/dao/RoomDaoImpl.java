7dfa534c0391d9d79a40252c87b4e6037f7cc216
Daeheum <conquerer1209@gmail.com>
2020-11-13 16:34:52 +0900

    [EDIT] modify room status Real-time update
    Signed-off-by: Daeheum <conquerer1209@gmail.com>

--- gameBack_InGame/src/main/java/com/ssafy/pjt3/dao/RoomDaoImpl.java
+++ gameBack_InGame/src/main/java/com/ssafy/pjt3/dao/RoomDaoImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.pjt3.dto.Room;
 import com.ssafy.pjt3.dto.User;
 
+@Repository
 public class RoomDaoImpl implements RoomDao{
 	@Autowired
 	private SqlSession session;
